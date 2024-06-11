0fc10a22d8884e194afbf970119dfddb6c35dab3
wiktor-falek <falekwiktor@gmail.com>
2024-06-11 04:57:31 +0200

    .

--- cslox/Program.cs
+++ cslox/Program.cs
@@ -57,12 +57,12 @@
     }
   }
 
-  static void Error(int line, string message)
+  public static void Error(int line, string message)
   {
     Report(line, "", message);
   }
 
-  static void Report(int line, string where, string message)
+  public static void Report(int line, string where, string message)
   {
     Console.Error.WriteLine($"[line {line}] Error {where}: {message}");
     hadError = true;
--- cslox/lib/Scanner.cs
+++ cslox/lib/Scanner.cs
@@ -27,7 +27,47 @@ public class Scanner(string source)
       case '+': AddToken(TokenType.PLUS); break;
       case ';': AddToken(TokenType.SEMICOLON); break;
       case '*': AddToken(TokenType.STAR); break;
+      case '!':
+        AddToken(Match('=') ? TokenType.BANG_EQUAL : TokenType.BANG); break;
+      case '=':
+        AddToken(Match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
+      case '<':
+        AddToken(Match('=') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
+      case '>':
+        AddToken(Match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+      case '/':
+        if (Match('/'))
+        {
+          while (Peek() != '\n' && !IsAtEnd()) Advance();
+        }
+        else
+        {
+          AddToken(TokenType.SLASH);
+        }
+        break;
+      case ' ':
+      case '\r':
+      // TODO: wtf is carriage return
+      case '\t':
+        break;
+      case '\n':
+        Line++;
+        break;
+      case '"': String(); break;
+      default:
+        Lox.Error(Line, $"Unexpected character: {c}"); break;
     }
+  }
+
+  private void String()
+  {
+    // TODO
+  }
+
+  private char Peek()
+  {
+    if (IsAtEnd()) return '\0';
+    return Source.ElementAt(Current);
   }
 
   private char Advance()
@@ -45,11 +85,19 @@ public class Scanner(string source)
     string text = Source.Substring(Start, Current);
     Token token = new(type, text, literal, Line);
     _ = Tokens.Append(token);
-    // TODO: update current
   }
 
   private bool IsAtEnd()
   {
     return Current > Source.Length;
+  }
+
+  private bool Match(char expected)
+  {
+    if (IsAtEnd()) return false;
+    if (Source.ElementAt(Current) != expected) return false;
+
+    Current++;
+    return true;
   }
 }
