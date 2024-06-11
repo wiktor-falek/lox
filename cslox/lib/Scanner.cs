5e86897f870a72ccc8a4b1d8b07998a859b2f6b5
wiktor-falek <falekwiktor@gmail.com>
2024-06-11 04:14:34 +0200

    scanner

--- cslox/lib/Scanner.cs
+++ cslox/lib/Scanner.cs
@@ -1,10 +1,55 @@
 public class Scanner(string source)
 {
-  string Source = source;
+  private string Source = source;
+  private List<Token> Tokens = [];
+  private int Start = 0;
+  private int Current = 0;
+  private int Line = 1;
 
   public List<Token> ScanTokens()
   {
     List<Token> arr = [];
     return arr;
+  }
+
+  private void ScanToken()
+  {
+    char c = Advance();
+    switch (c)
+    {
+      case '(': AddToken(TokenType.LEFT_PAREN); break;
+      case ')': AddToken(TokenType.RIGHT_PAREN); break;
+      case '{': AddToken(TokenType.LEFT_BRACE); break;
+      case '}': AddToken(TokenType.RIGHT_BRACE); break;
+      case ',': AddToken(TokenType.COMMA); break;
+      case '.': AddToken(TokenType.DOT); break;
+      case '-': AddToken(TokenType.MINUS); break;
+      case '+': AddToken(TokenType.PLUS); break;
+      case ';': AddToken(TokenType.SEMICOLON); break;
+      case '*': AddToken(TokenType.STAR); break;
+    }
+  }
+
+  private char Advance()
+  {
+    return Source.ElementAt(Current++);
+  }
+
+  private void AddToken(TokenType token)
+  {
+    AddToken(token, null);
+  }
+
+  private void AddToken(TokenType type, object? literal)
+  {
+    string text = Source.Substring(Start, Current);
+    Token token = new(type, text, literal, Line);
+    _ = Tokens.Append(token);
+    // TODO: update current
+  }
+
+  private bool IsAtEnd()
+  {
+    return Current > Source.Length;
   }
 }
--- cslox/lib/Token.cs
+++ cslox/lib/Token.cs
@@ -2,10 +2,10 @@ public class Token
 {
   readonly TokenType Type;
   readonly string Lexeme;
-  readonly object Literal;
+  readonly object? Literal;
   readonly int Line;
 
-  Token(TokenType type, string lexeme, object literal, int line)
+  public Token(TokenType type, string lexeme, object? literal, int line)
   {
     Type = type;
     Lexeme = lexeme;
