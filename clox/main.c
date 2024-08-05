ce80c4fc891d88ec93966ff872dc7785070dd1fe
wiktor-falek <falekwiktor@gmail.com>
2024-08-05 23:52:59 +0200

    disassemble chunk

--- clox/main.c
+++ clox/main.c
@@ -1,5 +1,6 @@
 #include "chunk.h"
 #include "common.h"
+#include "debug.h"
 #include <stdio.h>
 
 int main(int argc, const char *argv[])
@@ -7,6 +8,8 @@ int main(int argc, const char *argv[])
 	Chunk chunk;
 	init_chunk(&chunk);
 	write_chunk(&chunk, OP_RETURN);
+
+	disassemble_chunk(&chunk, "test chunk");
 	free_chunk(&chunk);
 	return 0;
 }
