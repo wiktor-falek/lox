ba69f6f5c514219dd402b7cedef2ae7aacb89c8e
wiktor-falek <falekwiktor@gmail.com>
2024-08-06 16:24:45 +0200

    store lines in chunk

--- clox/main.c
+++ clox/main.c
@@ -7,11 +7,11 @@ int main(int argc, const char *argv[])
 	Chunk chunk;
 	init_chunk(&chunk);
 
-	write_chunk(&chunk, OP_RETURN);
+	write_chunk(&chunk, OP_RETURN, 0);
 
 	int constant = add_constant(&chunk, 1.2);
-	write_chunk(&chunk, OP_CONSTANT);
-	write_chunk(&chunk, constant);
+	write_chunk(&chunk, OP_CONSTANT, 0);
+	write_chunk(&chunk, constant, 0);
 
 	disassemble_chunk(&chunk, "test chunk");
 	free_chunk(&chunk);
